b0fc54093419a6cb07dceb40275da1feda522da6
Tancredi Bernasconi <tancredi.bernasconi@heig-vd.ch>
2025-09-16 16:09:24 +0200

    Travail terminé

---
+++ rendu/hello.c
@@ -0,0 +1,11 @@
+/**
+* My /**
+* My first program in C
+* Author: Tancredi Bernasconi <tancredi.bernasconi@heig-vd.ch>
+**/
+#include <stdio.h>
+
+int main() {
+   printf("hello, world\n");
+   return 0;
+}
